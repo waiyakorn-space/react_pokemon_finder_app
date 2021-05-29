5a2f51f0a9bcf9d3212587e45f786760e949987c
Waiyakorn Phoomjarownwat <waiyakorn.krow@gmail.com>
2021-05-30 00:25:22 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -10,27 +10,29 @@ export default class App extends Component {
       err:""
     }
   }
+
   componentDidMount(){
     this.getPokemon(this.state.query)
   }
+
   getPokemon = async () => {
     try{
-      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.query.toLocaleLowerCase()}`)
-      const data = await res.json();
-      // console.log(res);
-      console.log('data',data);
-      this.setState({
-        pokemon:data,
-        err:null
-      })
-
+      if(this.state.query !== ""){
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.query.toLocaleLowerCase()}`)
+        const data = await res.json();
+        // console.log(res);
+        console.log('data',data);
+        this.setState({
+          pokemon:data,
+          err:null
+        })
+      }else{this.setState({err : "empty"})}
     }catch(err){
       this.setState({
         pokemon:null,
         err
       })
-      console.log(err)
-      
+      console.log('error',err)
     }
   }
 
@@ -44,19 +46,27 @@ export default class App extends Component {
   }
 
   render() {
-    console.log(this.state.query)
+    let{query, pokemon, err} = this.state;
+    console.log('query',this.state.query,query)
+
     return (
           <>
           <div className="main-div">
             <form onSubmit={this.handleSubmit}>
               <h3>Search Pokemon</h3>
-              <input type="text" onChange={this.handleChange} value={this.state.query} />
+              <input type="text" onChange={this.handleChange} value={query} />
               <input type="submit" value="Search" />
             </form>    
-            {this.state.pokemon && !this.state.err ? (
+            {pokemon && !err ? (
             <div className="pokemon-pic">
-            <img src={`https://pokeres.bastionbot.org/images/pokemon/${this.state.pokemon.id}.png`} alt={this.state.pokemon.name} />
-            <h2 className="pokemon-name-title">{this.state.pokemon.name}</h2>
+            <img src={`https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png`} alt={pokemon.name} />
+            <h2 className="pokemon-name-title">{pokemon.name}</h2>
+            <ul>
+            <li>Type : {pokemon.types[0]['type'].name}</li>
+            <li>Weight : {pokemon.weight}   Height: {pokemon.height} </li>
+            <li>Ability 1 : {pokemon.abilities[0].ability.name}</li>
+            <li>Ability 2 : {pokemon.abilities[1].ability.name}</li>
+            </ul>
             </div>
             ) : (            
             <div className="error">
